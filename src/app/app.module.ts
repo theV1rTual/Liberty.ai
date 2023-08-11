4956bef19b21ec03d34da23ce06e16a391ae8b28
Arystan <71083612+theV1rTual@users.noreply.github.com>
2023-08-11 19:47:10 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./components/header/header.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FooterComponent} from "./components/footer/footer.component";
+import {RequestDemoComponent} from "./components/request-demo/request-demo.component";
+import {TechnologiesComponent} from "./components/technologies/technologies.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RequestDemoComponent,
+    TechnologiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/request-demo/request-demo.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-request-demo',
+  templateUrl: './request-demo.component.html'
+})
+
+export class RequestDemoComponent {
+
+}
